640c7004c1d8f92c0b4579383826f9063af32772
César González <cesargonzaleziniguez@teamitg.com>
2025-02-23 12:28:07 +0100

    playoff styles

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Section } from '../../constants/section.enum';
 import { LeagueTeam } from './interfaces/league.interface';
 import { PlayoffRound } from './interfaces/playoff.interface';
 
@@ -11,11 +12,11 @@ export class HttpService {
   private readonly _http: HttpClient = inject(HttpClient);
   private readonly _baseUrl = 'http://localhost:3000/';
 
-  public getLeague(groupId: number, section = 'm'): Observable<LeagueTeam[]> {
+  public getLeague(groupId: number, section = Section.Masculino): Observable<LeagueTeam[]> {
     return this._http.get<LeagueTeam[]>(`${this._baseUrl}league/${groupId}/section/${section}`);
   }
 
-  public getPlayoff(groupId: number): Observable<PlayoffRound[]> {
-    return this._http.get<PlayoffRound[]>(`${this._baseUrl}playoff/${groupId}`);
+  public getPlayoff(groupId: number, section = Section.Masculino): Observable<PlayoffRound[]> {
+    return this._http.get<PlayoffRound[]>(`${this._baseUrl}playoff/${groupId}/section/${section}`);
   }
 }
